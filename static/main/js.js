0c1e4643d012f15fe9419b4720d6203328b425d1
Irka-Probirka <ankuznecov42@yandex.ru>
2022-12-16 15:58:42 +0500

    смена темы для основной страницы

--- static/main/js.js
+++ static/main/js.js
@@ -37,10 +37,27 @@ block[0].addEventListener('click', function () {
         switcher[0].style.left = '2.25em';
         //
         // смена страницы на Светлую
-        document.body.style.color = '#000000';
-        document.getElementsByClassName('header')[0].style.backgroundColor = '#fff';
-        document.getElementsByClassName('header')[0].style.color = '#000000';
-        document.getElementsByClassName('main-content')[0].style.backgroundColor = '#ECF3F5';
+        document.body.classList.add('light-theme-body');
+        document.querySelector('.main-content').classList.add('light-theme-body2');
+
+        // header
+        document.querySelector('.header').classList.add('light-theme-header-footer');
+        document.querySelectorAll('ul:not(.dropdown-menu) li')
+            .forEach((el) => el.classList.add('light-theme-header-footer'));
+        document.querySelectorAll('ul:not(.dropdown-menu) li :is(a)')
+            .forEach((el) => el.classList.add('light-theme-header-footer'));
+
+        // footer
+        document.querySelector('.footer').classList.add('light-theme-header-footer');
+
+        // основные блоки
+        // document.querySelector('.content').classList.add('light-theme-content');
+        document.querySelectorAll('.name-items:not(#active)').forEach((el) => {
+           el.classList.add('light-theme-content');
+        });
+        document.querySelectorAll('.some-info').forEach((el) => {
+           el.classList.add('light-theme-content');
+        });
     }
     else {
         switcher[0].style.backgroundColor = '#1f1f1f';
@@ -48,10 +65,27 @@ block[0].addEventListener('click', function () {
         switcher[0].style.left = '.25em';
         //
         // смена страницы на Темную
-        document.body.style.color = '#888888';
-        document.getElementsByClassName('header')[0].style.backgroundColor = '#383838';
-        document.getElementsByClassName('header')[0].style.color = '#000';
-        document.getElementsByClassName('main-content')[0].style.backgroundColor = '#1f1f1f';
+        document.body.classList.remove('light-theme-body');
+        document.querySelector('.main-content').classList.remove('light-theme-body2');
+
+        // header
+        document.querySelector('.header').classList.remove('light-theme-header-footer');
+        document.querySelectorAll('ul:not(.dropdown-menu) li')
+            .forEach((el) => el.classList.remove('light-theme-header-footer'));
+        document.querySelectorAll('ul:not(.dropdown-menu) li :is(a)')
+            .forEach((el) => el.classList.remove('light-theme-header-footer'));
+
+        // footer
+        document.querySelector('.footer').classList.remove('light-theme-header-footer');
+
+        // основные блоки
+        // document.querySelector('.content').classList.remove('light-theme-content');
+        document.querySelectorAll('.name-items:not(#active)').forEach((el) => {
+           el.classList.remove('light-theme-content');
+        });
+        document.querySelectorAll('.some-info').forEach((el) => {
+           el.classList.remove('light-theme-content');
+        });
     }
 
     count_clicker = count_clicker + 1;
